4d253078a7ac1e6f7e872ccd92b87e5a0756efca
Amol023 <amol5jsh@gmail.com>
2016-07-11 15:32:54 -0700

    chore cleaned index files

--- client/controllers/HomeController.js
+++ client/controllers/HomeController.js
@@ -1,4 +1,5 @@
 appPlayer.controller('HomeController', ['$scope', 'socket',
+<<<<<<< 2e1e8f1db9cbebad033db4b5e0cac5ab671bd353
         function($scope, socket) {
             // Sound manager is a audio player library with hundreds of methods available,
             // The setup we have should be enough for a MVP.
@@ -110,3 +111,87 @@ appPlayer.controller('HomeController', ['$scope', 'socket',
             }
         };
     });
+=======
+  function($scope, socket) {
+    /// chat controller stuff
+    $scope.user = false;
+    $scope.typing = false;
+    $scope.TYPING_TIMER_LENGTH = 2000; // this is how quick the "[other user] is typing" message will go away
+    $scope.chatSend = function(){
+      socket.emit('chat message', $scope.chatMsg);  
+      $scope.chatMsg = "";
+      return false;
+    }
+  
+    $scope.chatMessages = [];
+
+    socket.on('chat message', function(msg){
+      console.log('on is listening');
+      $scope.chatMessages.push(msg);
+    });
+
+    $scope.setName = function(){
+      $scope.user = true;
+      socket.emit('username', $scope.screenName);
+    };
+
+    socket.on("playNpause", function(obj){
+      console.log('we heard you', obj);
+    });
+
+    $scope.updateTyping = function (){
+      $scope.typing = true;
+      socket.emit('typing', $scope.name);
+      var lastTypingTime = (new Date()).getTime();
+
+      setTimeout(function () {
+        var typingTimer = (new Date()).getTime();
+        var timeDiff = typingTimer - lastTypingTime;
+        if (timeDiff >= $scope.TYPING_TIMER_LENGTH && $scope.typing) {
+          socket.emit('stop typing');
+          $scope.typing = false;
+        }
+      }, $scope.TYPING_TIMER_LENGTH);
+    };
+
+    // Whenever the server emits 'typing', show the typing message
+    socket.on('typing', function (data) {
+      data.typing = true;
+      $scope.typingMessage = data.name + " is typing";
+      if(!$scope.chatMessages.includes($scope.typingMessage)){
+        $scope.chatMessages.push($scope.typingMessage);
+      }
+    });
+
+    // Whenever the server emits 'stop typing', kill the typing message
+    socket.on('stop typing', function (data) {
+      data.typing = false;
+      var i = $scope.chatMessages.indexOf($scope.typingMessage);
+      $scope.chatMessages.splice(i, 1);
+    });
+
+}]);  
+.factory('socket', function($rootScope){
+  var socket = io.connect();
+  return{
+    on: function(eventName, callback){
+      socket.on(eventName, function(){
+        var args = arguments;
+        $rootScope.$apply(function(){
+        callback.apply(socket, args);
+        });
+      });
+    },
+    emit: function(eventName, data, callback){
+      socket.emit(eventName, data, function(){
+        var args = arguments;
+        $rootScope.$apply(function(){
+          if(callback){
+            callback.apply(socket, args);
+          }
+        })
+      })
+    }
+  };
+});
+>>>>>>> chore cleaned index files
